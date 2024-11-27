d417885e9dc17489a9d7b3c4fedff69e049fc110
Weerawut <weerawut@live.com>
2024-11-27 14:13:31 +0700

    Initial Commit

---
+++ OddOneOut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OddOneOut
+//
+//  Created by Weerawut Chaiyasomboon on 27/11/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by Weerawut Chaiyasomboon on 27/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
